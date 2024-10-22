37ae0e06f41e67cfeb2f6fc79786b70386407f56
Ron <80211681+rongplus@users.noreply.github.com>
2024-10-22 01:21:12 +0000

    v1

---
+++ pages/视频.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("This is the 感悟 title")
+st.header("This is the 感悟")
+st.markdown("This is the markdown")
+st.subheader("This is the subheader")
+st.caption("This is the caption")
+st.code("x = 2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
